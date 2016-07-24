74f979132fdc969859939e699f5d199e35efe237
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2016-07-24 17:17:41 +0100

    First commit

---
+++ src/main/java/org/jbduncan/Application.java
@@ -0,0 +1,8 @@
+package org.jbduncan;
+
+public final class Application {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    new RuntimeException();
+  }
+}
---
+++ src/test/java/org/jbduncan/ApplicationTest.java
@@ -0,0 +1,11 @@
+package org.jbduncan;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApplicationTest {
+  @Test
+  public void main() throws Exception {
+    Assert.assertTrue("message", 2 + 2 == 4);
+  }
+}
